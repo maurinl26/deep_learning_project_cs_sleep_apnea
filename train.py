c95da3cd403e35960ceb24e4fb1b59c2368b1dba
Loic Maurin <maurin.loic.ac@gmail.com>
2021-02-09 23:48:30 +0100

    Numpy files as input Data Loader changed

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import h5py
-import glob
-
 from sklearn.model_selection import train_test_split
 
 import torch
@@ -12,19 +9,18 @@ import torch.utils.data as data
 from unet import UNet
 from low_pass_filter import low_pass_filter
 
-class SleepApneaDataset(torch.utils.data.Dataset):
+# DataLoader
+class Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, X_PATH, Y_PATH):
-        self.file = h5py.File(X_PATH,'r')
-        self.labels = pd.read_csv(Y_PATH)
+    def __init__(self, labels, samples):
+        self.labels = labels
+        self.samples = samples
 
     def __len__(self):
-        return self.file['data'].shape[0]
+        return len(self.labels)
 
     def __getitem__(self, index):
-        X = np.array(self.file.get('data'))
-        y = self.labels[index]
-        return (X[index, 2:], y)
+        return self.samples[index], self.labels[index]
 
 
 if __name__ == "__main__":
@@ -35,31 +31,28 @@ if __name__ == "__main__":
     # Loading data
     X_TRAIN_LABELS_PATH = "./data/X_train_h7ipJUo.csv"
     Y_TRAIN_PATH = "./data/y_train_tX9Br0C.csv"
-    X_TRAIN_PATH = "./data/X_train.h5"
+    X_TRAIN_PATH = "./data/X_train.npy"
 
-    X_train_labels = pd.read_csv(X_TRAIN_LABELS_PATH)
-    train_dataset = h5py.File(X_TRAIN_PATH)
-    y_train = pd.read_csv(Y_TRAIN_PATH)
+    X_train_labels = pd.read_csv(X_TRAIN_LABELS_PATH).to_numpy()
+    X_train = torch.from_numpy(np.load(X_TRAIN_PATH)).float()
+    y_train = torch.from_numpy(pd.read_csv(Y_TRAIN_PATH).to_numpy()).float()
+
 
     # First to columns are removed
-    X_train = train_dataset['data'][:, 2:]
+    X_train = X_train[:, 2:]
 
     # Reshape to a dataset with 8 channels in 3rd dimension
     X_train = X_train.reshape(4400, 8, 9000)
 
-    # Application of a low pass filter to 10Hz to remove noise
-    # Cut off frequency to play with
-    # X_train = low_pass_filter(X_train, 10) # Not working
-
     # Train test split (could be improved in k fold validation)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=0.8, random_state=42)
 
-    train_set = SleepApneaDataset(X_TRAIN_PATH, Y_TRAIN_PATH)
+    # train_set = SleepApneaDataset(X_TRAIN_PATH, Y_TRAIN_PATH)
 
     BATCH_SIZE = 220
 
     train_loader = torch.utils.data.DataLoader(
-        dataset=train_set,
+        dataset=Dataset(y_train, X_train),
         batch_size=BATCH_SIZE,
         shuffle=True,
         num_workers=4
@@ -73,7 +66,7 @@ if __name__ == "__main__":
     )
 
     # Def of network
-    model = UNet(in_channels=8, out_channels=1)
+    model = UNet(in_channels=8, out_channels=1, features=9000)
     model.to(device)
 
     # Optimizer : Adam
@@ -109,7 +102,7 @@ if __name__ == "__main__":
                 # _, prediction = torch.max(out.data, 1)
                 prediction = out.argmax(dim=1, keepdim=True) # index of the max log-probability
                 correct += prediction.eq(target.view_as(prediction)).sum().item()
-        taux_classif = 100. * correct / len(test_loader.dataset)
+        taux_classif = 100. * correct / len(val_loader.dataset)
         print('Accuracy: {}/{} (tx {:.2f}%, err {:.2f}%)\n'.format(correct,
         len(val_loader.dataset), taux_classif, 100.-taux_classif))
 
