c8bab0c9841c70143f4bfc4440338caa50c08be7
Loic Maurin <maurin.loic.ac@gmail.com>
2021-02-11 22:50:10 +0100

    UNet Implemented with 5 encoder and a final classifier

--- unet.py
+++ unet.py
@@ -2,97 +2,163 @@ from collections import OrderedDict
 
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class UNet(nn.Module):
 
-    def __init__(self, in_channels=8, out_channels=1, init_features=32):
+    def __init__(self, channels, features):
         super(UNet, self).__init__()
 
-        features = init_features
-        self.encoder1 = UNet._block(in_channels, features, name="enc1")
-        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(features, features * 2, name="enc2")
-        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
-        self.pool3 = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
-        self.pool4 = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.enc1 = nn.Sequential(
+            nn.Conv1d(channels, 16, 5, padding=2),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
 
-        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+            nn.Conv1d(16, 16, 5, padding=2),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True)
+        )
 
-        self.upconv4 = nn.ConvTranspose1d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose1d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose1d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose1d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+        self.enc2 = nn.Sequential(
+            nn.MaxPool1d(10),
+            nn.Conv1d(16, 32, 5, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
 
-        self.conv = nn.Conv1d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
+            nn.Conv1d(32, 32, 5, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True)
         )
+
+        self.enc3 = nn.Sequential(
+            nn.MaxPool1d(8),
+            nn.Conv1d(32, 64, 5, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(64, 64, 5, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.enc4 = nn.Sequential(
+            nn.MaxPool1d(6),
+            nn.Conv1d(64, 128, 5, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(128, 128, 5, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True)
+        )
+
+        self.enc5 = nn.Sequential(
+            nn.MaxPool1d(4),
+            nn.Conv1d(128, 256, 5, padding=2),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(256, 256, 5, padding=2),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True)
+        )
+
+        self.decoder_1 = nn.Sequential(
+            nn.Upsample(18),
+            nn.Conv1d(256, 128, 5, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True)
+        )
+
+        self.decoder_2 = nn.Sequential(
+            nn.Conv1d(256, 128, 5, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(128, 128, 5, padding=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Upsample(113),
+            nn.Conv1d(128, 64, 6, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True)
+        )
+
+        self.decoder_3 = nn.Sequential(
+            nn.Conv1d(128, 64, 5, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(64, 64, 5, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+
+            nn.Upsample(903),
+            nn.Conv1d(64, 32, 8, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True)
+        )
+
+        self.decoder_4 = nn.Sequential(
+            nn.Conv1d(64, 32, 5, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(32, 32, 5, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+
+            nn.Upsample(9005),
+            nn.Conv1d(32, 16, 10, padding=2),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+        )
+
+        self.decoder_5 = nn.Sequential(
+            nn.Conv1d(32, 16, 5, padding=2),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+
+            nn.Conv1d(16, 16, 5, padding=2),
+            nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
+        )
+
+        self.fully_conn1 = nn.Sequential(
+            nn.Linear(9000, 900),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fully_conn2 = nn.Sequential(
+            nn.Linear(900, 90),
+            nn.Softmax()
+        )
+
 
     def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
 
-        bottleneck = self.bottleneck(self.pool4(enc4))
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(enc1)
+        enc3 = self.enc3(enc2)
+        enc4 = self.enc4(enc3)
+        enc5 = self.enc5(enc4)
 
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
+        x = self.decoder_1(enc5)
+        x = torch.cat((x, enc4), 1)
 
-    @staticmethod
-    def _block(in_channels, features, name):
-        return nn.Sequential(
-            OrderedDict(
-                [
-                    (
-                        name + "conv1",
-                        nn.Conv1d(
-                            in_channels=in_channels,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm1", nn.BatchNorm1d(num_features=features)),
-                    (name + "relu1", nn.ReLU(inplace=True)),
-                    (
-                        name + "conv2",
-                        nn.Conv1d(
-                            in_channels=features,
-                            out_channels=features,
-                            kernel_size=3,
-                            padding=1,
-                            bias=False,
-                        ),
-                    ),
-                    (name + "norm2", nn.BatchNorm1d(num_features=features)),
-                    (name + "relu2", nn.ReLU(inplace=True)),
-                ]
-            )
-        )
+        x = self.decoder_2(x)
+        x = torch.cat((x, enc3), 1)
+
+        x = self.decoder_3(x)
+        x = torch.cat((x, enc2), 1)
+
+        x = self.decoder_4(x)
+        x = torch.cat((x, enc1), 1)
+
+        print(x.shape)
+        # Ajouter classifier avec
+        x = self.fully_conn1(x)
+        x = self.fully_conn2(x)
+
+        return x
