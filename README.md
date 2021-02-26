b98cb15cd2ceda473cd100318e46b922a4fdb485
Loic Maurin <maurin.loic.ac@gmail.com>
2021-02-26 20:13:53 +0100

    train.py renamed in train_unet.py MFCC + CNN implemented

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Sleep Apnea Detection
 Project from Deep Learning class, at CentraleSupelec : Detecting sleep apnea from multivariate timeseries.
 
-##
+## Challenge Data ENS - Dreem Sleep Apnea Detection
+
+
 
---
+++ mfcc_cnn.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class CNNNet(nn.Module):
+    def __init__(self):
+        super(CNNNet).__init__()
+
+        return None
+
+    def forward(self, x):
+
+        return None
